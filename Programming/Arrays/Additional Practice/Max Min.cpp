550718b0e66f26e6741b6836dd170e1b6f43bd30
Akash Sahu <akkusahu559@gmail.com>
2021-03-13 11:58:14 +0530

    Another approach added into Java

--- Programming/Arrays/Additional Practice/Max Min.cpp
+++ Programming/Arrays/Additional Practice/Max Min.cpp
@@ -12,3 +12,16 @@ int Solution::solve(vector<int> &A) {
 }
 
 
+// 2nd Approach in Java
+// Time Complexity = O(N), Space Complexity = O(1)
+
+public int solve(ArrayList<Integer> A) {
+    int max = -999999999;
+	int min = 999999999;
+    for(int itr = 0; itr < A.size(); itr++){
+        max = Math.max(A.get(itr), max);
+        min = Math.min(A.get(itr), min);
+    }
+    return max+min;
+}
+
